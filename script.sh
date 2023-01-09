f6250776c09649e651dae4ba2e1f3ce0966c27c7
tomdeporte <57455352+tomdeporte@users.noreply.github.com>
2023-01-09 16:55:57 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,16 +2,16 @@
 
 cd /home/gradesadmin
 
-apt-get install ansible -y
+sudo apt-get install ansible -y
 
-mkdir /root/ansible
+sudo mkdir /root/ansible
 
-echo -e "[defaults]\ninventory=/root/ansible/inventory\nremote_user=root\nhost_key_checking=False\nbecome=True\nbecome_user=root\nbecome_ask_pass=False\n" >> /root/ansible/ansible.cfg
+sudo echo -e "[defaults]\ninventory=/root/ansible/inventory\nremote_user=root\nhost_key_checking=False\nbecome=True\nbecome_user=root\nbecome_ask_pass=False\n" >> /root/ansible/ansible.cfg
 
-echo -e "[apache]\n192.168.0.100\n[mariadb]\n192.168.0.101" >> /root/ansible/inventory
+sudo echo -e "[apache]\n192.168.0.100\n[mariadb]\n192.168.0.101" >> /root/ansible/inventory
 
 export ANSIBLE_CONFIG=/root/ansible/ansible.cfg
 
-echo "export ANSIBLE_CONFIG=/root/ansible/ansible.cfg" >> ~/.profile
+sudo echo "export ANSIBLE_CONFIG=/root/ansible/ansible.cfg" >> ~/.profile
 
 source ~/.profile
