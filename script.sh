8310f092542320735d339efcded59559f5def8ee
tomdeporte <57455352+tomdeporte@users.noreply.github.com>
2023-01-10 14:42:02 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-cd /home/gradesadmin
-
-apt-get install ansible -y
-
 FOLDER=/root/ansible
 if [ -d "$FOLDER" ]; then
-    echo "$FOLDER exists."
+    echo "Ansible is already installed"
 else 
     sudo mkdir /root/ansible
+    
+    cd /home/gradesadmin
+
+    apt-get install ansible -y
+
+    echo -e "[defaults]\ninventory=/root/ansible/inventory\nremote_user=root\nhost_key_checking=False\nbecome=True\nbecome_user=root\nbecome_ask_pass=False\n" >> /root/ansible/ansible.cfg
+
+    echo -e "[apache]\n192.168.0.100\n[mariadb]\n192.168.0.101" >> /root/ansible/inventory
+
+    export ANSIBLE_CONFIG=/root/ansible/ansible.cfg
+
+    echo "export ANSIBLE_CONFIG=/root/ansible/ansible.cfg" >> ~/.profile
+
+    source ~/.profile
 fi
 
-echo -e "[defaults]\ninventory=/root/ansible/inventory\nremote_user=root\nhost_key_checking=False\nbecome=True\nbecome_user=root\nbecome_ask_pass=False\n" >> /root/ansible/ansible.cfg
 
-echo -e "[apache]\n192.168.0.100\n[mariadb]\n192.168.0.101" >> /root/ansible/inventory
-
-export ANSIBLE_CONFIG=/root/ansible/ansible.cfg
-
-echo "export ANSIBLE_CONFIG=/root/ansible/ansible.cfg" >> ~/.profile
-
-source ~/.profile
